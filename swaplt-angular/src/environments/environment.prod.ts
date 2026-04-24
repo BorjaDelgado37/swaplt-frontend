311c15ffae742e47f22e9c4ebaac889f0753b672
Borja Delgado Sanchez <140327252+BorjaDelgado37@users.noreply.github.com>
2026-04-24 02:26:31 +0200

    prueba2.1

--- swaplt-angular/src/environments/environment.prod.ts
+++ swaplt-angular/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://laravel.compra-venta.duckdns.org/api', // URL real de producción de la VPS
+  apiUrl: 'https://swaplt-backend-production.up.railway.app/api', // URL real de producción de la VPS
   googleMapsApiKey: 'AIzaSyByvl71BOmk14aAarLbzDpHQrE8kW0igug', // Usar la misma clave que en desarrollo
   googleTranslateApiKey: 'AIzaSyAMY8sX9zAq2pKrREO_mByynsbVAsfOaJ0' // Debes reemplazar con tu clave real de Google Translate API
 };
